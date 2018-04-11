37d9963b33da536859af325385e712c62fdad9b1
encisooliver <oliverenciso07@gmail.com>
2018-04-11 10:00:34 +0800

    Complete CRUD

--- innosoft-ph-api/ApiControllers/ApiMstArticleController.cs
+++ innosoft-ph-api/ApiControllers/ApiMstArticleController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -15,17 +16,158 @@ namespace innosoft_ph_api.ApiControllers
         [HttpGet, Route("list/{articleTypeId}")]
         public List<Entities.MstArticle> ListArticle(String articleTypeId)
         {
-            var articles = from d in db.MstArticles
+            var articles = from d in db.MstArticles.OrderByDescending(d => d.Id)
                            where d.ArticleTypeId == Convert.ToInt32(articleTypeId)
                            select new Entities.MstArticle
                            {
                                Id = d.Id,
                                ArticleCode = d.ArticleCode,
                                ManualArticleCode = d.ManualArticleCode,
-                               Article = d.Article
+                               Article = d.Article,
+                               Category = d.Category,
+                               ArticleTypeId = d.ArticleTypeId,
+                               ArticleGroupId = d.ArticleGroupId,
+                               AccountId = d.AccountId,
+                               SalesAccountId = d.SalesAccountId,
+                               CostAccountId = d.CostAccountId,
+
                            };
 
             return articles.ToList();
+        }
+
+        [HttpGet, Route("detail/{id}")]
+        public Entities.MstArticle DetailArticle(String id)
+        {
+            var article = from d in db.MstArticles.OrderByDescending(d => d.Id)
+                          where d.Id == Convert.ToInt32(id)
+                          select new Entities.MstArticle
+                          {
+                              Id = d.Id,
+                              ArticleCode = d.ArticleCode,
+                              ManualArticleCode = d.ManualArticleCode,
+                              Article = d.Article,
+                              Category = d.Category
+                          };
+
+            return article.FirstOrDefault();
+        }
+
+        [HttpPost, Route("add")]
+        public HttpResponseMessage AddArticle(Entities.MstArticle objArticle)
+        {
+            try
+            {
+                Data.MstArticle newArticle = new Data.MstArticle
+                {
+                    ArticleCode = objArticle.ArticleCode,
+                    ManualArticleCode = objArticle.ManualArticleCode,
+                    Article = objArticle.Article,
+                    Category = objArticle.Category,
+                    ArticleTypeId = objArticle.ArticleTypeId,
+                    ArticleGroupId = objArticle.ArticleGroupId,
+                    AccountId = objArticle.AccountId,
+                    SalesAccountId = objArticle.SalesAccountId,
+                    CostAccountId = objArticle.CostAccountId,
+                    AssetAccountId = objArticle.AssetAccountId,
+                    ExpenseAccountId = objArticle.ExpenseAccountId,
+                    UnitId = objArticle.UnitId,
+                    OutputTaxId = objArticle.OutputTaxId,
+                    InputTaxId = objArticle.InputTaxId,
+                    WTaxTypeId = objArticle.WTaxTypeId,
+                    Price = objArticle.Price,
+                    Cost = objArticle.Cost,
+                    IsInventory = objArticle.IsInventory,
+                    Particulars = objArticle.Particulars,
+                    Address = objArticle.Address,
+                    TermId = objArticle.TermId,
+                    ContactNumber = objArticle.ContactNumber,
+                    ContactPerson = objArticle.ContactPerson,
+                    EmailAddress = objArticle.EmailAddress,
+                    TaxNumber = objArticle.TaxNumber,
+                    CreditLimit = objArticle.CreditLimit,
+                    DateAcquired = Convert.ToDateTime(objArticle.DateAcquired),
+                    UsefulLife = objArticle.UsefulLife,
+                    SalvageValue = objArticle.SalvageValue,
+                    ManualArticleOldCode = objArticle.ManualArticleOldCode,
+                    Kitting = objArticle.Kitting,
+                    IsLocked = objArticle.IsLocked,
+                    CreatedById = objArticle.CreatedById,
+                    CreatedDateTime = Convert.ToDateTime(objArticle.CreatedDateTime),
+                    UpdatedById = objArticle.UpdatedById,
+                    UpdatedDateTime = Convert.ToDateTime(objArticle.UpdatedDateTime),
+                };
+
+                db.MstArticles.InsertOnSubmit(newArticle);
+                db.SubmitChanges();
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Successfully Added!");
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Something's went wrong!");
+            }
+        }
+
+        [HttpPut, Route("update/{id}")]
+        public HttpResponseMessage UpdateArticle(Entities.MstArticle objArticle, String id)
+        {
+            try
+            {
+                var article = from d in db.MstArticles
+                              where d.Id == Convert.ToInt32(id)
+                              select d;
+
+                if (article.Any())
+                {
+                    var updateArticle = article.FirstOrDefault();
+                    updateArticle.Article = objArticle.Article;
+                    updateArticle.Category = objArticle.Category;
+
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK, "Successfully Updated!");
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, "Data Not Exist!");
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Something's went wrong!");
+            }
+        }
+
+        [HttpDelete, Route("delete/{id}")]
+        public HttpResponseMessage DeleteArticle(String id)
+        {
+            try
+            {
+                var article = from d in db.MstArticles
+                              where d.Id == Convert.ToInt32(id)
+                              select d;
+
+                if (article.Any())
+                {
+                    db.MstArticles.DeleteOnSubmit(article.First());
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK, "Successfully Deleted!");
+
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, "Data Not Exist!");
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Something's went wrong!");
+            }
         }
     }
 }
--- innosoft-ph-api/Entities/MstArticle.cs
+++ innosoft-ph-api/Entities/MstArticle.cs
@@ -11,5 +11,38 @@ namespace innosoft_ph_api.Entities
         public String ArticleCode { set; get; }
         public String ManualArticleCode { set; get; }
         public String Article { set; get; }
-    }
+        public String Category { set; get; }
+        public Int32 ArticleTypeId { set; get; }
+        public Int32 ArticleGroupId { set; get; }
+        public Int32 AccountId { set; get; }
+        public Int32 SalesAccountId { set; get; }
+        public Int32 CostAccountId { set; get; }
+        public Int32 AssetAccountId { set; get; }
+        public Int32 ExpenseAccountId { set; get; }
+        public Int32 UnitId { set; get; }
+        public Int32 OutputTaxId { set; get; }
+        public Int32 InputTaxId { set; get; }
+        public Int32 WTaxTypeId { set; get; }
+        public Decimal Price { set; get; }
+        public Decimal Cost { set; get; }
+        public Boolean IsInventory { set; get; }
+        public String Particulars { set; get; }
+        public String Address { set; get; }
+        public Int32 TermId { set; get; }
+        public String ContactNumber { set; get; }
+        public String ContactPerson { set; get; }
+        public String EmailAddress { set; get; }
+        public String TaxNumber { set; get; }
+        public Decimal CreditLimit { set; get; }
+        public String DateAcquired { set; get; }
+        public Decimal UsefulLife { set; get; }
+        public Decimal SalvageValue { set; get; }
+        public String ManualArticleOldCode { set; get; }
+        public Int32 Kitting { set; get; }
+        public Boolean IsLocked { set; get; }
+        public Int32 CreatedById { set; get; }
+        public String CreatedDateTime { set; get; }
+        public Int32 UpdatedById { set; get; }
+        public String UpdatedDateTime { set; get; }
+    }   
 }
